7b629b2d7a8cac1b0e2e72ddf019b893c8046f52
masumdkd <masumdkd@gmail.com>
2023-08-09 11:48:33 +0600

    4th

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
-
+from .views import HomeView, PostView, AddPostView, UpdatePostView
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', HomeView.as_view(), name="home"),
+    path('post/<int:pk>', PostView.as_view(), name="post" ), 
+    path('post/add', AddPostView.as_view(), name="add_post" ), 
+    path('post/edit/<int:pk>', UpdatePostView.as_view(), name="update_post" ), 
 ]
+
+
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Post
+from .forms import PostForm
 
-def home(request):
-    return render(request, 'home.html', {})
+# def home(request):
+#     return render(request, 'home.html', {})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class PostView(DetailView): 
+    model = Post
+    template_name = 'post.html'
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+
+class UpdatePostView(UpdateView):
+    model= Post
+    form_class = PostForm
+    template_name = 'update_post.html'
