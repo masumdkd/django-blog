254ffc5b8d32b2da1b6877e737ada8212a037bd4
masumdkd <masumdkd@gmail.com>
2023-08-20 11:04:35 +0600

    seven

--- blog/settings.py
+++ blog/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "/home/mdmasum/django-blog/static"
+STATIC_ROOT = "/home/mdmasum/static"
 
 
 LOGIN_REDIRECT_URL = 'home'
